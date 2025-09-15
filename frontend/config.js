2b5f3ffe2447b24f6b467583624ce8c111a3d814
PierreMarie59491 <pierre.marie.59491@gmail.com>
2025-09-16 00:11:10 +0200

    directory changes 2

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 // URL du backend Render
-window.API_BASE = 'window.API_BASE = 'https://concert-ehpad-1.onrender.com'; // ton backend Render';
+window.API_BASE = 'https://concert-ehpad.onrender.com';
--- frontend/public/config.js
+++
@@ -1,2 +0,0 @@
-// URL du backend Render
-window.API_BASE = 'https://concert-ehpad.onrender.com';
